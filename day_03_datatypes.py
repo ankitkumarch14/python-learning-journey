6f15bc3c71d63973ca3d853dc421b90278983281
ankitkumarch14 <ankitkumarch14@gmail.com>
2026-07-21 17:19:31 +0530

    Add files via upload

---
+++ day_03_datatypes.py
@@ -0,0 +1,14 @@
+name = "Ankit"        # String
+age = 22              # Integer
+cgpa = 7.53           # Float
+is_student = True     # Boolean
+
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
